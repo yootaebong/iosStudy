0463cacb7870d022f6d754e7f4938a01eb4a9774
System Administrator <root@taebongui-MacBookPro.local>
2020-02-14 22:19:24 +0900

    apple tutorial List project

---
+++ Landmark_appDeveloper/StartingPoint/Landmarks/Landmarks/LandmarkList.swift
@@ -0,0 +1,34 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by taebong you on 2020/02/14.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)){
+                    LandmarkRow(landmark: landmark)
+                }
+                
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE", "iPhone XS Max","iPhone 8"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+
+    }
+}
